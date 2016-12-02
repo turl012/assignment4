4a9bc2168b2d8de165c4b649259e9aeb8faa88a3
Matthew Turley <matthew.a.turley@gmail.com>
2016-12-01 21:46:46 -0500

    Have basics done, need style

--- assignment4.js
+++ assignment4.js
@@ -15,3 +15,54 @@
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
 })();
+
+$('#searchBox').keyup(function() {
+  $('#results').empty();
+
+  var query = $('#searchBox').val();
+
+  if(query.length == 0) {
+    $('#results').removeClass('notEmpty');
+    return;
+  }
+
+  var mattAPI  = 'http://www.mattbowytz.com/simple_api.json?data=all';
+  var mattAPI2 =  'http://www.mattbowytz.com/simple_api.json?data=comics';
+
+  var listOut = '';
+
+  $.ajax({
+    dataType: 'json',
+    url: mattAPI,
+    success: function(data) {
+      $.each(data.data, function(index, column) {
+        $.each(column, function(index2, value) {
+          if(value.substring(0, query.length).toLowerCase() == query.toLowerCase()) {
+            listOut += value
+          }
+        });
+      });
+
+      $.ajax({
+        dataType: 'json',
+        url: mattAPI2,
+        success: function(data) {
+          $.each(data.data, function(index3, value2) {
+            if(value2.substring(0, query.length).toLowerCase() == query.toLowerCase()) {
+              listOut += value2;
+            }
+          });
+
+          $('#results').html(listOut);
+
+          if($('#results').children().length < 1) {
+            $('#results').removeClass('notEmpty');
+          } else {
+            $('#results').addClass('notEmpty');
+          }
+        }
+      });
+    }
+  });
+
+});
